f2250ce2fc76d74f30396c7192dfac59ce3fd013
roger moore <rogermoore06@gmail.com>
2023-07-19 04:40:38 -0500

    Player complete, can shoot only AI and abrriers, movement constrained, hide cursor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private PlayerInputActions _input;
+
+    [SerializeField]
+    private GameObject _bulletHole;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        _input = new PlayerInputActions();
+        _input.Player.Enable();
+        _input.Player.Fire.performed += Fire_performed;
         //TODO: move the spawning to another script
         SpawnManager.Instance.StartSpawning();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Fire_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log("Fire performed");
+        Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin, out hit, Mathf.Infinity, 1 << 6 | 1 << 7))
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+        }
     }
 }
